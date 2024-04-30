9e6aaf0f2d875065282f5f92b5eb9460589da8c6
Thabang019 <thabangcebo@gmail.com>
2024-05-01 00:52:42 +0200

    phase one

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,25 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Posts') }}
         </h2>
     </x-slot>
 
+    <div class="max-w-4xl mx-auto mb-8">
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+            @foreach ($posts as $post)
+            <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                <h2 class="font-bold text-2xl">
+                    <a href="{{ route('posts.show', $post) }}">   {{$post->message}}</a>
+                </h2>
+                <p class="mt-2">
+                    {{ Str::limit($post->message, 200)}}
+                </p>
+                <span class="text-gray-800">Author : {{ $post->user->name }}</span>
+                <span class="block mt-4 text-sm opacity-70">{{ $post->created_at->format('j M Y, g:i a') }}</span>
             </div>
+            @endforeach
         </div>
     </div>
+
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,38 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post; // Import the Post model at the top of your file
 
 Route::get('/', function () {
-    return view('welcome');
+    $posts = Post::with('user')->latest()->get();
+    return view('welcome',['posts' => $posts]);
 });
 
+
+
+Route::get('/dashboard', function () {
+    $posts = Post::with('user')->latest()->get(); // Retrieve posts with associated users
+    return view('dashboard', ['posts' => $posts]); // Pass the $posts variable to the 'dashboard' view
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+/*
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+*/
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('posts', PostController::class)
+    ->only(['index', 'store', 'show'])
+    ->middleware(['auth', 'verified']);
+
+
 
 require __DIR__.'/auth.php';
